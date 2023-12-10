777c3110f4c90a71fed45ee3d2bdefc39721ce1c
jiananli <33046262+jiananli306@users.noreply.github.com>
2023-12-10 18:34:23 -0500

    two channel enabled

--- AD774X_Init_V1_5/AD7746_base_function.ino
+++ AD774X_Init_V1_5/AD7746_base_function.ino
@@ -1,19 +1,22 @@
 //---------------------------------------------------------------------
-//all the base function used in AD7746
+// all the base function used in AD7746
 //---------------------------------------------------------------------
 
 //----------------------------------------------------------------------
-//TCA9548 select funtcion
-//tca select function 0->7, input i -> seriral port 0 to 7
-void tcaselect(uint8_t i) {
-  if (i > 7) return;
+// TCA9548 select funtcion
+// tca select function 0->7, input i -> seriral port 0 to 7
+void tcaselect(uint8_t i)
+{
+  if (i > 7)
+    return;
   Wire.beginTransmission(TCAADDR);
   Wire.write(1 << i);
-  Wire.endTransmission();  
+  Wire.endTransmission();
 }
 //---------------------------------------------------------------------
-//AD774X Reset funtion, command: 0xBF
-void AD774X_Reset() {
+// AD774X Reset funtion, command: 0xBF
+void AD774X_Reset()
+{
   Wire.beginTransmission(AD774X_ADDRESS);
   Wire.write(0xBF);
   I2C_State = Wire.endTransmission();
@@ -25,7 +28,8 @@ void AD774X_Reset() {
 // Output: content of the registry
 // Output: I2C_State - returns an error of I2C, 0 = without error
 //---------------------------------------------------------------------
-uint8_t AD774X_Read_Single_Register(uint8_t RegAdres) {
+uint8_t AD774X_Read_Single_Register(uint8_t RegAdres)
+{
   Wire.beginTransmission(AD774X_ADDRESS);
   Wire.write(RegAdres);
   I2C_State = Wire.endTransmission(false);
@@ -38,40 +42,46 @@ uint8_t AD774X_Read_Single_Register(uint8_t RegAdres) {
 // Input: DataSingl - written data
 // Output: I2C_State - returns an error of I2C, 0 = without error
 //----------------------------------------------------------------------
-void AD774X_Write_Single_Register(uint8_t RegAdres, uint8_t DataSingl) {
+void AD774X_Write_Single_Register(uint8_t RegAdres, uint8_t DataSingl)
+{
   Wire.beginTransmission(AD774X_ADDRESS);
   Wire.write(RegAdres);
   Wire.write(DataSingl);
   I2C_State = Wire.endTransmission();
 }
 //----------------------------------------------------------------------
-//conversion of the obtained data to real values
+// conversion of the obtained data to real values
 //----------------------------------------------------------------------
-void Read_raw(uint8_t channel, uint8_t channelIndex, uint8_t channelIndex_mask){
-    //Serial.println(F(" ,in loop 1 !"));
-    uint8_t channel_TCA = channel/2;
-    //Serial.println(channel);
-   // Serial.println(channel_TCA);
-    tcaselect(TCA_address[channel_TCA]);
-    Wire.beginTransmission(AD774X_ADDRESS);
-    Wire.write(ADR_CAP_DATAH);
-    I2C_State = Wire.endTransmission(false);
-    Wire.requestFrom(AD774X_ADDRESS, 3);
-    for (uint8_t i = 0; i < 3; i++)
-    {
-      output_total[channel][i] = Wire.read();
-    }
-    if(channel%2 == 0){
-    AD774X_Write_Single_Register(ADR_CAPDACA,B00110111);//code work for length demo, need adjust for real data
-    AD774X_Write_Single_Register(ADR_CAP_SETUP,DATA_CAP2_SETUP);//change to channel 2
-    AD774X_Write_Single_Register(ADR_EXC_SETUP,DATA_EXC2_SETUP);//change to channel 2
-    }
-    else{
-    AD774X_Write_Single_Register(ADR_CAP_SETUP,DATA_CAP_SETUP);//change to channel 2
-    AD774X_Write_Single_Register(ADR_CAPDACA,B10110111);//code work for length demo, need adjust for real data
-    AD774X_Write_Single_Register(ADR_EXC_SETUP,DATA_EXC_SETUP);//change to channel 2
-    }
-    floatOut[channel] =  (float)((long((long)output_total[channel][0] << 16) + ((long)output_total[channel][1] << 8) + (long)output_total[channel][2] - 0x800000) / 2048000.0);
-    totalFlag_done = (totalFlag_done | channelIndex);
-    totalFlag = totalFlag & channelIndex_mask;
+void Read_raw(uint8_t channel, uint8_t channelIndex, uint8_t channelIndex_mask)
+{
+  //Serial.println(channel);
+  uint8_t channel_TCA = channel / 2;
+  //change to desired channel
+  tcaselect(TCA_address[channel_TCA]);
+  Wire.beginTransmission(AD774X_ADDRESS);
+  Wire.write(ADR_CAP_DATAH);
+  I2C_State = Wire.endTransmission(false);
+  Wire.requestFrom(AD774X_ADDRESS, 3);
+  for (uint8_t i = 0; i < 3; i++)
+  {
+    output_total[channel][i] = Wire.read();
   }
+  //finish current data read
+  floatOut[channel] = (float)((long((long)output_total[channel][0] << 16) + ((long)output_total[channel][1] << 8) + (long)output_total[channel][2] - 0x800000) / 2048000.0);
+  //set desired Flags
+  totalFlag_done = (totalFlag_done | channelIndex);
+  totalFlag = totalFlag & channelIndex_mask;
+  //handle next data
+  if(channel %2 ==0){//handle next channel from same chip
+    AD774X_Write_Single_Register(ADR_CAP_SETUP, DATA_CAP2_SETUP); // change to channel 2
+    AD774X_Write_Single_Register(ADR_EXC_SETUP, DATA_EXC2_SETUP); // change to channel 2
+  }
+  else{//goto next chip
+    AD774X_Write_Single_Register(ADR_CAP_SETUP, DATA_CAP_SETUP); // change to channel 2
+    AD774X_Write_Single_Register(ADR_EXC_SETUP, DATA_EXC_SETUP); // change to channel 2
+    tcaselect(TCA_address[(channel/2)+1]);
+  }
+  if(channel == 3)tcaselect(TCA_address[0]);
+  AD774X_Write_Single_Register(ADR_CFG, (AD774X_Read_Single_Register(ADR_CFG) & MODES) | SINGLE);
+
+}
--- AD774X_Init_V1_5/AD774X_Init_V1_5.ino
+++ AD774X_Init_V1_5/AD774X_Init_V1_5.ino
@@ -14,7 +14,7 @@ const uint8_t TCA_address[] = {6, 5, 3, 2};           // TCA used i2c channel
 const uint8_t DATA_CAP_SETUP = B11100000;  // 7  CAPEN+CIN2+CAPDIF+0000+CAPCHOP according, default cin2 single mode, channel 1
 const uint8_t DATA_CAP2_SETUP = B10100000; // 7  CAPEN+CIN2+CAPDIF+0000+CAPCHOP according, default cin2 single mode, channel 2
 const uint8_t DATA_VT_SETUP = B00000001;   // 8  VTEN+VTMD1+VTMD0+EXTREF+00+VTSHORT+VTCHOP for internal temperature, default disable
-const uint8_t DATA_EXC_SETUP = B00001011;  // 9  CLKCTRL+EXCON+EXCB+EXCB^+EXCA+EXCA^+EXCLVL1+EXCLVL0, default EXCA 1/2Vdd
+const uint8_t DATA_EXC_SETUP =  B00001011;  // 9  CLKCTRL+EXCON+EXCB+EXCB^+EXCA+EXCA^+EXCLVL1+EXCLVL0, default EXCA 1/2Vdd
 const uint8_t DATA_EXC2_SETUP = B00100011; // 9  CLKCTRL+EXCON+EXCB+EXCB^+EXCA+EXCA^+EXCLVL1+EXCLVL0, default EXCA 1/2Vdd, channel 2
 const uint8_t DATA_CFG = B11111000;        // 10 VTF1+VTF0+CAPF2+CAPF1+CAPF0+MD2+MD1+MD0 , VT conversion time 8.2Hz, Cap conversion time 90.9Hz,Idel time
 const uint8_t DATA_CAPDACA = B00000000;    // 11 CAPDACA OFF
@@ -51,28 +51,18 @@ uint8_t totalFlag_done = B00000000;
 //-------------------------------------------------------------------
 const uint8_t Channel[8] = {B00000001, B00000010, B00000100, B00001000, B00010000, B00100000, B01000000, B10000000};
 const uint8_t Channel_mask[8] = {B11111110, B11111101, B11111011, B11110111, B11101111, B11011111, B10111111, B01111111};
-//
+//-------------------------------------------------------------------
 const uint8_t MODES = B11111000;      // 0xF8 is "AND" mask for preset convert mode
 const uint8_t SINGLE = B00000010;     // 0x02 is "OR" mask for start single convert mode
 const uint8_t CONTIN = B00000001;     // 0x01 is "OR" mask for start continual convert mode
+const uint8_t IDLE = B00000000;       // 0x00 is "OR" mask for start IDLE mode
 const uint8_t CAPDAC_ON = B10000000;  // 0x80 is "OR" mask for CAPDAC ON
 const uint8_t CAPDAC_OFF = B00000000; // 0x00 is value for CAPDAC OFF
 const uint8_t CAP_RDY = B00000001;    // 0x01 is "AND" mask for CAP READY
 const uint8_t REFERENCE = B11101111;  // 0xEF is "AND" mask for unconditionally set internal reference
 const uint8_t OneByte = 1;            // auxiliary variables
-unsigned long TimeTemp = 0;
-unsigned long TimeTempInt = 0;
-unsigned long TimeTempOut = 0; // auxiliary variables for timing
-volatile uint8_t RTxBuff[20];
-volatile uint8_t RTxBuff2[20];                                  // I/O buffer for AD774X registers
 uint8_t I2C_State = 0;                                          // status of I2C bus, 0 = without error
-unsigned int SamplePeriod = 1;                                  // sample period in [ms]
-float C1 = 0, C2 = 0;                                           // auxiliary variables for zero correction calculation
-float Capacitance = 0.0, Capacitance2 = 0.0, Temperature = 0.0; // real data
-bool EnablePeriodicSampling = false;                            // periodic sampling with output to serial port, is default disabled
-// const uint8_t DefaultRegisters[] PROGMEM = {0, 0, 0, 0, 0, 0, 0, DATA_CAP_SETUP, DATA_VT_SETUP, DATA_EXC_SETUP, DATA_CFG,
-//                                            DATA_CAPDACA, DATA_CAPDACB, DATA_CAP_OFFH, DATA_CAP_OFFL, 0, 0, 0, 0
-//                                         };
+
 //----------------------------------------------------------------------
 // declare Arduino reset function at address 0
 //----------------------------------------------------------------------
@@ -112,28 +102,15 @@ void setup()
       AD774X_Write_Single_Register(ADR_CAPDACB, DATA_CAPDACB);
       AD774X_Write_Single_Register(ADR_CAP_OFFH, DATA_CAP_OFFH);
       AD774X_Write_Single_Register(ADR_CAP_OFFL, DATA_CAP_OFFL);
-      if (i == 0)
-      {
-        attachInterrupt(digitalPinToInterrupt(CAP_INTERRUPT_PIN[i]), ReadNewData_1, FALLING);
-      }
-      if (i == 1)
-      {
-        attachInterrupt(digitalPinToInterrupt(CAP_INTERRUPT_PIN[i]), ReadNewData_2, FALLING);
-      }
-      if (i == 2)
-      {
-        attachInterrupt(digitalPinToInterrupt(CAP_INTERRUPT_PIN[i]), ReadNewData_3, FALLING);
-      }
-      if (i == 3)
-      {
-        attachInterrupt(digitalPinToInterrupt(CAP_INTERRUPT_PIN[i]), ReadNewData_4, FALLING);
-      }
+      if (i == 0){attachInterrupt(digitalPinToInterrupt(CAP_INTERRUPT_PIN[i]), ReadNewData_1, FALLING);}
+      if (i == 1){attachInterrupt(digitalPinToInterrupt(CAP_INTERRUPT_PIN[i]), ReadNewData_2, FALLING);}
+      if (i == 2){attachInterrupt(digitalPinToInterrupt(CAP_INTERRUPT_PIN[i]), ReadNewData_3, FALLING);}
+      if (i == 3){ attachInterrupt(digitalPinToInterrupt(CAP_INTERRUPT_PIN[i]), ReadNewData_4, FALLING);}
     }
   }
+  //Start Channel 0 cont mode
   tcaselect(TCA_address[0]);
-  AD774X_Write_Single_Register(ADR_CFG, (AD774X_Read_Single_Register(ADR_CFG) & MODES) | CONTIN);
-
-  // StartNewConversion();
+  AD774X_Write_Single_Register(ADR_CFG, (AD774X_Read_Single_Register(ADR_CFG) & MODES) | SINGLE);
 }
 //----------------------------------------------------------------------
 void loop()
@@ -146,7 +123,7 @@ void loop()
     }
   }
  //generate output
-  if (totalFlag_done == B00000011)
+  if (totalFlag_done == B00001111)//change this to B11111111 when all enabled
   {
     for (int8_t j = 0; j < 8; j++)
     {
